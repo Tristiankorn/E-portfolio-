d570ba51f10f648734c899031789eb17dbc3fcc7
TristianKorn <tristiankorn@icloud.com>
2024-02-26 08:36:32 -0700

    Im going over the medium challenges one more time then headeding on to hard challenges

--- Desktop/app.js M challenges/app.js
+++ Desktop/app.js M challenges/app.js
@@ -1,5 +1,5 @@
-// Q1. Falsey or truthy? 
-// - given two values, return the first one if it is falsey, otherwise return the second value 
+// Q1. Falsey or truthy?
+// - given two values, return the first one if it is falsey, otherwise return the second value
 // function filterOutFalsey(num1, num2) {
 // if (num1 === false) {
 //     return num1
@@ -14,8 +14,6 @@
 
 // console.log(filterOutFalsey(200, 300))
 
-
-
 // Q2. Return the length of any given array || arr.length method
 // function arrLength(arr) {
 //    return arr.length
@@ -23,42 +21,36 @@
 
 // console.log(arrLength([]))
 
-
-
-// Q3. Get the last element of an array 
+// Q3. Get the last element of an array
 // function lastElem(arr) {
 // return arr[arr.length - 1]
 // }
 
 // console.log(lastElem([23, 34, null, "Tristian"]))
 
-
-
 // Q4. Find the sum of an array
 // function getMax(arr) {
 //   let max = 0;
 //   for (let i = 0; i < arr.length; ++i) {
-//     max = max + arr[i]; 
+//     max = max + arr[i];
 // }
 // return max;
 // }
 // console.log(getMax([234, 411, 137]))
 
-
-// Q5.) Add up the numbers from a single given number 
+// Q5.) Add up the numbers from a single given number
 // function progressiveSum(elem) {
 //     let sum = 0;
 //     for (let i = 1; i <= elem; ++i) {
 //         sum = sum + i;
 //     }
 // return sum;
-    
+
 // }
 
 // console.log(progressiveSum(4))
 
-
-// Q6. Calculate the time 
+// Q6. Calculate the time
 // function calcTime(seconds) {
 //     let timerMinutes = Math.floor(seconds / 60);
 //     let timerSeconds = seconds % 60
@@ -72,8 +64,7 @@
 // }
 // console.log(calcTime(50))
 
-
-// Q7. FInd the largest number 
+// Q7. FInd the largest number
 
 // function getMax(arr) {
 // let max = arr[0];
@@ -86,7 +77,6 @@
 // }
 
 // console.log(getMax([-300, 0, 23, 435]));
-
 
 // Q8. Reverse a string  || .reverse ||
 
@@ -107,7 +97,6 @@
 
 // console.log(reverseString('tristianhasbigpeepee'))
 
-
 //  Q9A. Turn every element in an aray into 0 || soloution 1 - For loop ||
 // function convertToZero(arr) {
 //     let newArr = []
@@ -118,14 +107,11 @@
 // }
 // console.log(convertToZero([9, 3, 33, 88]))
 
-
-
 //  Q9B. Turn every element in an aray into 0 || soloution 2 - arr.fill ||
 // function convertToZeros(arr) {
 //     return new Array(arr.length).fill(0);
 // }
 // console.log(convertToZeros([1, 2, 4, 5]))
-
 
 // Q9C. Turn every element in an aray into 0 || soloution 3 - arr.map ||
 // function convertToZeros(arr) {
@@ -144,20 +130,17 @@
 // }
 // console.log(convertToZeros([12, 0, 12, 34,]))
 
-
 // function convertToZeros(arr) {
 // return new Array(arr.length).fill(0)
 // }
 
 // console.log(convertToZeros([12, 0, 12,]))
 
-
 // function convertToZeros(arr) {
 // return arr.map(elem => 'doggy wang')
 // }
 
 // console.log(convertToZeros([12, 0, 12, 34,]))
-
 
 // Q10A. remove a targeted element in an array
 // function removeApples(arr) {
@@ -170,13 +153,41 @@
 // }
 // console.log(removeApples(['Banannas', 'Apples', 'Melons', 'Berries', 'Apples']));
 
+// 10B. remove a targeted element from an arr || using arr.filter
+
+// function noApples(arr) {
+// return arr.filter(elem => elem !== 'Apples');
+
+// }
+
+// console.log(noApples(['Banannas', 'Apples', 'Melons', 'Berries', 'Apples']))
 
 
-// 10B. 
-function noApples(arr) {
-return arr.filter(elem => elem !== 'Apples');
 
-}
 
-console.log(noApples(['Banannas', 'Apples', 'Melons', 'Berries', 'Apples']))
+
+//  kade and Tristian prctice (medium diffiuclty)
+
+// Q1. Falsey or truthy?
+// - given two values, return the first one if it is falsey, otherwise return the second value
+// function trueOrFalse(elem1, elem2) {
+//     return elem1 || elem2;
+// }
+// console.log(trueOrFalse(0 , 500))
+
+// function filterFalsey(elem1, elem2) {
+
+// }
+
+// console.log(filterFalsey())
+
+// Q2. Return the length of any given array || arr.length method
+// k - & T
+// function arrayLength(arr){
+//     for (let i = 0; i < arr.length; ++i) {
+//         console.log(arr[i])
+//     }
+// }
+
+// console.log(arrayLength(['Hello', 'KAde', 'Tristian', 23]))
 
