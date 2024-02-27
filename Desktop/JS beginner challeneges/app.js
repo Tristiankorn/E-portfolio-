244e73a0ebea51c7ad828cb90d991d382648d945
TristianKorn <tristiankorn@icloud.com>
2024-02-27 15:45:32 -0700

    I am redoing the beginner Java - Script challenges. So far I was able to do 11/11 but I still have 3more to do. once I can do these all by myselg I will do the same process for medium & advanced challenges

--- Desktop/JS beginner challeneges/app.js
+++ Desktop/JS beginner challeneges/app.js
@@ -110,9 +110,101 @@
 // console.log(isLoggedInAndSubscribed('LOGGED_IN','SUBSCRIBED'))
 
 // || Q14.) Check if a user us logged in OR subsribed
-function isLoggedInAndSubscribed(loggedin, subscribed) {
-return (loggedin === 'LOGGED_IN' || subsribed === 'SUBSRCIBED')
+// function isLoggedInAndSubscribed(loggedin, subscribed) {
+// return (loggedin === 'LOGGED_IN' || subsribed === 'SUBSRCIBED')
  
-}
+// }
 
-console.log(isLoggedInAndSubscribed('LOGGED_IN', 'SUBSCRIBED'))
+// console.log(isLoggedInAndSubscribed('LOGGED_IN', 'SUBSCRIBED'))
+
+// 1.)    
+// function numberR(num1, num2) {
+//     return num1 + num2
+// }
+
+// console.log(numberR(3, 2))
+
+
+// 2.)
+// function hoursIntoSeconds(hour) {
+// return hour * 60 * 60
+// }
+
+// console.log(hoursIntoSeconds(10))
+
+
+// 3.) calculate the perimeter of a rectangle 
+// function calcPerimeter(length, width) {
+// return 2 * length + 2 * width;
+// }
+
+// console.log(calcPerimeter(20, 10))
+
+
+// 4.) calculate the area of a triangle 
+// function calcTriangleArea(base, height) {
+// return (0.5) * base * height 
+// }
+
+// console.log(calcTriangleArea(10, 10))
+
+
+// 5.) Extend a string
+// function appendFrontend(string) {
+// return string + 'Frontend';
+//  }
+
+// console.log(appendFrontend('Orange'))
+
+
+// 6.) greater than 100 
+// function sumGreaterThan100(num1, num2) {
+// return num1 + num2 > 100
+// }
+
+// console.log(sumGreaterThan100(20,99))
+
+
+// 7.) Less than or equal to zero?
+// function lessThanOrEqualToZero(num1) {
+// return num1 <= 0;
+// }
+
+// console.log(lessThanOrEqualToZero(3))
+
+
+// 8.) Opposte boolean 
+// function oppositeBoolean(bool) {
+// return !bool 
+// }
+
+// console.log(oppositeBoolean(true))
+
+
+// 9.) is not the number zero 
+// function isNotZero(elem) {
+//     return elem != 0
+// }
+
+// console.log(isNotZero(5))
+
+
+// 10.) Calculate the remainder
+// function calcRemainder(num1, num2) {
+// return num1 % num2
+// }
+
+// console.log(calcRemainder(7, 9))
+
+
+// 11.) Is the number Odd
+// function isOdd(num) {
+//     return num % 2 === 0;
+//     }
+    
+//     console.log(isOdd(8))
+
+
+// 12.) 
+
+
