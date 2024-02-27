ac725b2155f2e7411da8c2677ca2ce83b315989a
TristianKorn <tristiankorn@icloud.com>
2024-02-26 17:01:57 -0700

    I have started the hard challanges, Im working on going through the bootcamp video with an empahsis in understanding the Java - Script logic rather then memorizing all the mumbp jumbo syntax. Ill be working on this for 4 - 5 days most likely before I start my Advanced E - portfolio

---
+++ Desktop/app.js H challenges/app.js
@@ -0,0 +1,32 @@
+// function getArrayLength(arr) {
+//     for (let i = 0; i < arr.length; ++i) {
+//         console.log(arr[i])
+//     }
+
+// }
+
+// console.log(getArrayLength([1, 2, 3, 4, 'hello']))
+
+
+// Q1. Show rating
+
+// function showRating(rating) {
+//     let ratings = "";
+// for (let i = 0; i < Math.floor(rating); ++i) {
+//     ratings += "*"
+//     if (i !== Math.floor(rating) - 1) {
+//         ratings += " "
+//     }
+// }   
+// if ( !Number.isInteger(rating)) {
+//     ratings += " .";
+// }
+
+// return ratings;
+// }
+// console.log(showRating(3.5))
+
+
+
+
+   
