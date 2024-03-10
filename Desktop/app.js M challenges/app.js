cefb65d78d4b7f9a21fbab4090c8014866847f6d
TristianKorn <tristiankorn@icloud.com>
2024-03-10 11:35:31 -0700

    I coded for a good amount today, I was trying to take my time and undertsand again whats going on here what is the JS logic required to solve this type of problem, and what exactly is being exectuted line by line

--- Desktop/app.js M challenges/app.js
+++ Desktop/app.js M challenges/app.js
@@ -1,62 +1,4 @@
 
-// return appleRemover
-// }
-// console.log(removeApples(['Banannas', 'Apples', 'Melons', 'Berries', 'Apples']));
-
-// 10B. remove a targeted element from an arr || using arr.filter
-
-// function noApples(arr) {
-// return arr.filter(elem => elem !== 'Apples');
-
-// }
-
-// console.log(noApples(['Banannas', 'Apples', 'Melons', 'Berries', 'Apples']))
-
-
-
-
-
-//  kade and Tristian prctice (medium diffiuclty)
-
-
-
-// function noApples(arr) {
-// return arr.filter(elem => elem !== 'Apples')
-// }
-
-// console.log(noApples(['Watermelons', 'Apples', 'Peaches', 'Plums', 'Apples', 'Drleonard']))
-
-
-// 11A.
-// function filterOutFalsey(arr){
-//     return arr.filter(elem => !!elem === true)
-// }
-
-// console.log(filterOutFalsey([null, 0, "", 'Greetings', undefined, 234]))
-
-// 11A.
-// function filterOutFalsey(arr) {
-// let truthyArr = []
-//   for (let i = 0; i < arr.length; ++i) {
-//     if (!!arr[i] === true) {
-//         truthyArr.push(arr[i])
-//     }
-// }
-// return truthyArr
-
-// }
-
-// console.log(filterOutFalsey([null, 0, "", 'Greetings', undefined, 234]))
-
-
-// 12A Truthy to true, Falsey to false || arr.map method
-
-// function convertToBoolean(arr) {
-// return arr.map(elem => !!elem)
-// }
-
-// console.log(convertToBoolean([500, 'hello', [], 0, null, NaN, ""]))
-
 
 // Q1.) falsey or truthy - given two values, return thr first one if it is falsey, otherwise return the second one
 
@@ -219,11 +161,154 @@
 //   console.log(noApples(['Apples', 'Melons', 'PineApples', 'Oranges', 'Bananas']));
   
 
+// function noApples(arr) {
+// return arr.filter(elem => elem !== 'Apples')
+// }
 
-function noApples(arr) {
-return arr.filter(elem => elem !== 'Apples')
+// console.log(noApples(['Apples', 'melons', 'pineApples', 'Apples', 'Bannanas']))
+
+
+
+// 11A filter out all the falsey values
+
+// arr.filter 
+
+// function filterOutFalsey(arr) {
+//     return arr.filter(elem => !!elem === true)
+// }
+
+// console.log(filterOutFalsey([null, 'hello', 'apples', 23, '']))
+
+
+// 11B 
+
+// for loop 
+
+// function filterOutFalsey(arr) {
+//     let truthyArr = [];
+//     for (let i = 0; i < arr.length; ++i) {
+//         if (!!arr[i] === true) {
+//             truthyArr.push(arr[i])
+//         }
+//     }
+// return truthyArr
+            
+// }
+// console.log(filterOutFalsey([0, 'river', null,  "", 23]))
+
+
+
+// Q12A Truthy to true, Falsey to false || !arr.map!
+
+// function convertToBoolean(arr) {
+//     return arr.map(elem => !!elem)
+// }
+
+// console.log(convertToBoolean(['hi', null, NaN, 21, [2], '']))
+
+// Q1.) given two values, determine truthy or falsey 
+
+// function truthyOrFalsey(elem1, elem2) {
+// return !elem1 ? elem1 : elem2
+// }
+// console.log(truthyOrFalsey(0, 43))
+
+// Q2.) reeturn the length of any given array
+
+// function arrLength(arr) {
+//     for (let i = 0; i < arr.length; ++i) {
+//      console.loh(arr[i])
+//     }
+// }
+// console.log(arrLength([0, '', 'Trustian', null, 345, NaN]))
+
+
+// Q2B.) return the length of any given array
+
+// function arrLength(arr) {
+//     return arr.length;
+// }
+
+// console.log(arrLength([2, 343, 1, 345,]))
+
+
+// Q3A.) Get the last element in an array 
+
+// function lastElem(arr) {
+//     return arr[arr.length - 1]
+// }
+
+// console.log(lastElem([21, 34, 56, 'Assbackwards']))
+
+// Q4A.) 
+// function arrSum(arr) {
+// let sum = 0
+// for (let i = 0; i < arr.length ; ++i) {
+//      sum = sum + arr[i]
+// }
+// return sum 
+// }
+// console.log(arrSum([2, 45, -1256, 795]))
+
+// Q5.) Add up the numbers from a single number 
+// function progressiveSum(number) {
+//     let sum = 0
+// for (let i = 1; i <= number; ++i) [
+//  sum = sum + i
+// ]
+// return sum
+// }
+
+// console.log(progressiveSum(600))
+
+
+// Q6A.) calculate the time 
+// function calcTime(seconds) {
+
+// let timerMinutes = Math.floor(seconds / 60)
+// let timerSeconds = seconds % 60
+
+// if (timerMinutes.toString().length === 1) {
+//     timerMinutes = '0' + timerMinutes
+// }
+// return timerMinutes + ':' + timerSeconds;
+   
+    
+// }
+// console.log(calcTime(5000))
+
+
+// Q7. Find the largest number in an array
+// function getMax(arr) {
+// let max = arr[0]
+// for (let i = 0; i < arr.length; ++i) {
+//     if (arr[i] > max) {
+//         max = arr[i]
+//     }
+// }
+// return max;  
+// }
+// console.log(getMax([-300, -214, 0, 23, 453, 453]))
+  
+
+// Q8.) reverse a string || .reverse property 
+
+// function stringReverse(str) {
+// return str.split('').reverse().join('')
+// }
+
+// console.log(stringReverse('shmoopoop'))
+
+// 8B.) 
+
+function stringReverse(str) {
+let reversedString = '';
+for (let i = 0; i < str.length; ++i) {
+    reversedString = str[i] + reversedString;
+
+}
+return reversedString;
+    
 }
 
-console.log(noApples(['Apples', 'melons', 'pineApples', 'Apples', 'Bannanas']))
-
-
+console.log(reverseString('elloGovena'))
