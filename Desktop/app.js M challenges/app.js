25033b3a38dcd88ac5f145090314d3163f766276
TristianKorn <tristiankorn@icloud.com>
2024-03-18 15:47:15 -0700

    More practice today nothing new

--- Desktop/app.js M challenges/app.js
+++ Desktop/app.js M challenges/app.js
@@ -101,16 +101,97 @@
 // console.log(calcTime(2000))
 
 // Q7.) Return the largest number in an array
-function largestNum(arr) {
-    let max = []
-    for (let i = 0; i < arr.length; ++i) {
-        if (arr[i] > max) {
-            max = arr[i]
-        }
-    }
-    return max; 
-}
-console.log(largestNum([200, 400, -500]))
+// function largestNum(arr) {
+//     let max = []
+//     for (let i = 0; i < arr.length; ++i) {
+//         if (arr[i] > max) {
+//             max = arr[i]
+//         }
+//     }
+//     return max; 
+// }
+// console.log(largestNum([200, 400, -500]))
+
             
 
+//  Q8.) Reverse a String
+
+// function reverseString(str) {
+// return str.split('').reverse().join('')
+// }
+
+// console.log(reverseString('ABC'));
+
+// Q8B.) Reverse a string using for loop (incrementing)
+
+// function reverseString(str) {
+//     let reversedString = ':'
+//     for (let i = 0; i < str.length; ++i) {
+//         reversedString = reversedString + str[i]
+//     }
+//      return reversedString;   
+
+// }
+// console.log(reverseString('ABC'));
+
+// Q9A.) Turn every element in an array into zero (using arr.map)
+
+// function convertToZeros(arr) {
+// return arr.map(elem => 0)
+// }
+
+//  console.log(convertToZeros([12]))   
+
+// Q9B.) Turn every element in an array into zero (using arr.fill)
+
+// function convertToZeros(arr) {
+//      return new Array(arr.length).fill(0);
+//     }
+    
+//      console.log(convertToZeros([12, 2, 4]))   
+
+// Q9C.) Turn every element in an array into zero (using for loop)
+
+// function convertToZeros(arr) {
+//     let newArr = []; 
+//     for (let i = 0; i < arr.length; ++i) {
+//         newArr[i] = 0
+//     }
+// return newArr;
+// }
+// console.log(convertToZeros([0, 1, 3, 4, 5, 7]))
+
+// Q10A.) Filter out all the apples 
+// function noApples(arr) {
+//     return arr.filter(elem => elem !== 'Apples')
+// }
+// console.log(noApples(['Apples', 'Oranges', 'Blueberries', 'Cantelopes', 'Apples']))
+
+// Q10B.) Filter out all the apples (using a for loop)
+
+// function noApples(arr) {
+//     let appleRemover = []
+//     for (let i = 0; i < arr.length; ++i) {
+//         if (arr[i] !== 'Apples') {
+//             appleRemover.push(arr[i]);
+//         }
+//     }
+//     return appleRemover;
+// }
+
+// console.log(noApples(['Apples', 'Orages', 'Nectarines', 'Melons']))
+
+// function appleRemover(arr) {
+//     let noApples = []
+//     for (let i = 0; i < arr.length; ++i) {
+//         if (arr[i] !== 'Apples')
+//         noApples.push(arr[i]);
+//     }
+//     return noApples;
+// }
+// console.log(appleRemover(['Apples', 'Orages', 'Nectarines', 'Melons', 'Avocaods', 'Apples']))
+
+// Q11A.) filter out all the falsey values (For loop)
+
+function 
 
