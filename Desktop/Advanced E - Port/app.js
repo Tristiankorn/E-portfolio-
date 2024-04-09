eca087b3fc5e33d0d991ac96290f8f2358108a0d
TristianKorn <tristiankorn@icloud.com>
2024-04-09 14:35:46 -0700

    I finally added my functional java script to my modal form loading spiiner and pop up menu, tomorrow I will finish it up

--- Desktop/Advanced E - Port/app.js
+++ Desktop/Advanced E - Port/app.js
@@ -1,6 +1,23 @@
-function maxSum(arr) {
-        
-    
+// 
+// 
+// 
+
+function contact(event) {
+    event.preventDefault();
+    // emailjs
+    // .sendform(
+    //     'service_iexo6at',
+    //     'template_mnczjqc',
+    //     event.target,
+    //     'D8AzacTu3Aho9NZBE'
+    // ).then(() => {
+    //     console.log('this worked wooohooo')
+    // })
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible"
+    setTimeout(() => {
+        console.log('it worked 1')
+    }, 1000);
 }
 
-console.log([1, 2, 3, 4])
