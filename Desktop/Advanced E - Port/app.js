6f026cdffb50857095aa239d743275fe3a441b0a
TristianKorn <tristiankorn@icloud.com>
2024-04-11 11:53:35 -0700

    I was able to implement my emailjs code and get emials to go through however I seem to be targeting my css incorrectly becasue My button onclick is really messed up with color visibility going crazy. I cant figure it out so ill come back to this in a little

--- Desktop/Advanced E - Port/app.js
+++ Desktop/Advanced E - Port/app.js
@@ -1,23 +1,33 @@
-// 
-// 
-// 
+
 
 function contact(event) {
     event.preventDefault();
-    // emailjs
-    // .sendform(
-    //     'service_iexo6at',
-    //     'template_mnczjqc',
-    //     event.target,
-    //     'D8AzacTu3Aho9NZBE'
-    // ).then(() => {
-    //     console.log('this worked wooohooo')
-    // })
-    const loading = document.querySelector('.modal__overlay--loading')
-    const success = document.querySelector('.modal__overlay--success')
-    loading.classList += " modal__overlay--visible"
-    setTimeout(() => {
-        console.log('it worked 1')
-    }, 1000);
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList.add("modal__overlay--visible"); // Corrected
+    emailjs
+    .sendForm( // Corrected
+        'service_iexo6at',
+        'template_mnczjqc',
+        event.target,
+        'D8AzacTu3Aho9NZBE'
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList.add("modal__overlay--visible"); // Corrected
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me on tristiankorn@gmail.com" // Corrected
+        );
+    });
 }
 
+let isModalOpen = false;
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+  isModalOpen = true;
+  document.body.classList += " modal__open"
+}
